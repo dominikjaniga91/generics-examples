e18dbde645afa7e8dade0e68e383102ca825977a
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-20 18:45:44 +0100

    Change list raw type to parametrized type

---
+++ src/main/java/generify/MyList.java
@@ -0,0 +1,14 @@
+package generify;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MyList {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        String s = list.get(0);
+    }
+}
