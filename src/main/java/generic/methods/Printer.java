4e67dfe01cd843effaf41f730cf312981e461213
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-20 21:12:47 +0100

    Create generic method that print a list

---
+++ src/main/java/generic/methods/Printer.java
@@ -0,0 +1,19 @@
+package generic.methods;
+
+import java.util.List;
+
+class Printer {
+
+    /**
+     * {@code List<E> vs List<?> }
+     *
+     * {@code List<E>} is more powerful, for instance we can add element to that list.
+     * We can't do that in case of wildcard. However this method is responsible only for
+     * printing the elements of the list so unbounded wildcard is enough.
+     *
+     * @param elements the list of unknown type.
+     */
+    public static void printMe(List<?> elements) {
+        elements.forEach(System.out::println);
+    }
+}
