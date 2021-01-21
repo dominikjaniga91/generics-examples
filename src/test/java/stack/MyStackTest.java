371c9ad4525ecd8d09d0d5d73760bfb9822302f7
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-21 12:16:06 +0100

    Move creation of new MyStack() to setUp method

--- src/test/java/stack/MyStackTest.java
+++ src/test/java/stack/MyStackTest.java
@@ -1,6 +1,7 @@
 package stack;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.EmptyStackException;
@@ -9,12 +10,16 @@ import java.util.stream.IntStream;
 @Test(groups = "MyStack")
 public class MyStackTest {
 
-    public static final String STACK_ELEMENT = "stack element";
+    private static final String STACK_ELEMENT = "stack element";
+    private MyStack<String> stack;
+
+    @BeforeMethod
+    public void setUp() {
+        stack = new MyStack<>();
+    }
 
     @Test
     public void isEmptyShouldReturnTrue_whenStackIsEmpty() {
-        //given
-        MyStack<String> stack = new MyStack<>();
 
         //when
         boolean actual = stack.isEmpty();
@@ -26,93 +31,109 @@ public class MyStackTest {
 
     @Test
     public void newStackShouldHaveSizeEqualsToZero() {
-        MyStack<String> stack = new MyStack<>();
 
+        //when
         int actual = stack.size();
 
+        //then
         Assert.assertEquals(actual, 0, "The Stack should have size equals to zero");
     }
 
     @Test
     public void pushShouldReturnElement_afterAddToMyStack() {
-        MyStack<String> stack = new MyStack<>();
 
+        //when
         String actual = stack.push(STACK_ELEMENT);
 
+        //then
         Assert.assertEquals(actual, STACK_ELEMENT, "Push method should return added element");
     }
 
     @Test
     public void isEmptyShouldReturnFalse_whenStackIsNotEmpty() {
-        MyStack<String> stack = new MyStack<>();
+
+        //given
         stack.push(STACK_ELEMENT);
 
+        //when
         boolean actual = stack.isEmpty();
 
+        //then
         Assert.assertFalse(actual, "The Stack should not be empty");
     }
 
 
     @Test
     public void stackSizeShouldBeEqualsToOne_afterPushElement() {
-        MyStack<String> stack = new MyStack<>();
+
+        //given
         stack.push(STACK_ELEMENT);
+
+        //when
         int actual = stack.size();
 
+        //then
         Assert.assertEquals(actual, 1, "The Stack should have size equals to one");
     }
 
     @Test
     public void popShouldReturnElement_afterAddToMyStack() {
-        MyStack<String> stack = new MyStack<>();
+
+        //given
         stack.push(STACK_ELEMENT);
 
+        //when
         String actual = stack.pop();
 
+        //then
         Assert.assertEquals(actual, STACK_ELEMENT, "Push method should return added element");
     }
 
     @Test
     public void isEmptyShouldReturnTrue_afterPopElementFromMyStack() {
-        MyStack<String> stack = new MyStack<>();
+        //given
         stack.push(STACK_ELEMENT);
         stack.pop();
 
+        //when
         boolean actual = stack.isEmpty();
 
+        //then
         Assert.assertTrue(actual, "Stack should be empty");
     }
 
     @Test(expectedExceptions = EmptyStackException.class)
     public void popShouldThrowAnException_whenStackIsEmpty() {
-        MyStack<String> stack = new MyStack<>();
         stack.pop();
     }
 
     @Test
     public void peekShouldReturnElement_afterAddToMyStack() {
-        MyStack<String> stack = new MyStack<>();
+        //given
         stack.push(STACK_ELEMENT);
 
+        //when
         String actual = stack.peek();
 
+        //then
         Assert.assertEquals(actual, STACK_ELEMENT, "Peek method should return last element");
     }
 
     @Test(expectedExceptions = EmptyStackException.class)
     public void peekShouldThrowAnException_whenStackIsEmpty() {
-        MyStack<String> stack = new MyStack<>();
         stack.peek();
     }
 
     @Test
     public void shouldReturn25_afterIncreaseStackCapacity() {
+        //given
         MyStack<Integer> stack = new MyStack<>();
         IntStream.rangeClosed(1, 25).forEach(stack::push);
 
+        //when
         int actual = stack.size();
 
+        //then
         Assert.assertEquals(actual, 25, "Stack capacity did not increase");
     }
-
 }
