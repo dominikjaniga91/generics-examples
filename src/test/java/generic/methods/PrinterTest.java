9d8d8d1f810916f8354a4801b1fc9592ce87bab1
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-22 01:42:05 +0100

    Add test for print method

---
+++ src/test/java/generic/methods/PrinterTest.java
@@ -0,0 +1,31 @@
+package generic.methods;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Test(groups = "Teacher")
+public class PrinterTest {
+
+    @Test
+    public void drawShouldPrintValidHeader() {
+        //given
+        var people = List.of(new Person(1, "Jan", "Kowalski", LocalDate.now()),
+                             new Person(2, "Marek", "Kowalski", LocalDate.now()));
+        var byteStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteStream));
+        String expected = """
+                            Name: Jan, Surname: Kowalski
+                            Name: Marek, Surname: Kowalski
+                            """;
+        //when
+        Printer.printMe(people);
+
+        //then
+        Assert.assertEquals(byteStream.toString(), expected);
+    }
+}
