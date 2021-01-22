aa0f09667d18b59c2f8dc175b2c349cfcbd50e52
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-22 16:28:38 +0100

    Use putIfAbsent in Cache class

--- src/main/java/lru/cache/Cache.java
+++ src/main/java/lru/cache/Cache.java
@@ -7,39 +7,37 @@ class Cache<K, V> {
     private final int capacity;
     private Map<K,V> cache;
 
-    public Cache(int capacity) {
+    Cache(int capacity) {
         this.cache = new LinkedHashMap<>(capacity);
         this.capacity = capacity;
     }
 
-    public boolean empty() {
+    boolean empty() {
         return cache.isEmpty();
     }
 
-    public int size() {
+    int size() {
         return cache.size();
     }
 
-    public void store(K key, V value) {
-        if (this.cache.containsKey(key)) {
-            this.cache.remove(key);
-        } else if (this.cache.size() == this.capacity) {
+    void store(K key, V value) {
+        if (this.cache.size() == this.capacity) {
             Iterator<K> it = this.cache.keySet().iterator();
             it.next();
             it.remove();
         }
-        cache.put(key, value);
+        cache.putIfAbsent(key, value);
     }
 
-    public V seek(K key) {
+    V seek(K key) {
         return cache.get(key);
     }
 
-    public K firstKey() {
+    K firstKey() {
         return cache.keySet().stream().findFirst().orElse(null);
     }
 
-    public K lastKey() {
+    K lastKey() {
         Iterator<K> iterator = cache.keySet().iterator();
         K last = null;
         while (iterator.hasNext()) {
